3cc13318c3cf727119589fce3db2dbc3189b5594
AdeleM-prog <12043@holbertonstudents.com>
2026-01-21 09:55:30 +0000

    crea 0-square + 0-main + pycodestyle + chmod

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in matrix:
+        new_line = list(map(lambda x: x * x, i))
+        new_matrix.append(new_line)
+    return new_matrix
