4f7955c77964946327cc364479eb3c1b2b67de8f
AdeleM-prog <12043@holbertonstudents.com>
2026-01-21 17:48:37 +0000

    crea 12-main + 12-roman

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return None
+    else:
+    roman_list = {}
+    roman_list: {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+        }
+    return roman_list.values
