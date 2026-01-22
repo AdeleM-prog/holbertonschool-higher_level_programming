8d9e070679288c154a0428936e934dcb7d468869
AdeleM-prog <12043@holbertonstudents.com>
2026-01-22 15:44:39 +0000

    correction checker

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value), end="")
-        print()
+        print("{:d}".format(value))
         return True
-    except (ValueError):
-        print("ValueError, unknown format code")
-        print()
+    except (ValueError, TypeError):
         return False
