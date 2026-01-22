cb0e05a0639c34289582f9c2323c7b24885f612f
AdeleM-prog <12043@holbertonstudents.com>
2026-01-22 13:34:50 +0000

    correction checker

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     counter = 0
-    for x in my_list:
-        counter = counter + x
+    for i in range(x):
         try:
-            print("{}".format(x), end="")
-        except TypeError:
-            print("chaîne de format non prise en charge transmise à NoneType")
+            counter = counter + 1
+            print("{}".format(my_list[i]), end="")
+        except (IndexError):
+            counter = counter - 1
+            break
     print()
     return counter
