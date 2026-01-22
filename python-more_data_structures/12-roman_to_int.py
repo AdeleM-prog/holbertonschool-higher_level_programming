59e8f1b802fe779dfd826c33bd33d967fb920202
AdeleM-prog <12043@holbertonstudents.com>
2026-01-22 09:43:31 +0000

    Crea 12-main + 12-roman + pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
-        return None
-    else:
-    roman_list = {}
-    roman_list: {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000,
+    if not isinstance(roman_string, str):
+        return 0
+    if roman_string == "":
+        return 0
+    roman_list = {
+        "I": 1, "V": 5, "X": 10, "L": 50,
+        "C": 100, "D": 500, "M": 1000
         }
-    return roman_list.values
+    total = 0
+    for idx in range(len(roman_string) - 1):
+        current_value = roman_list[roman_string[idx]]
+        next_value = roman_list[roman_string[idx + 1]]
+        if current_value < next_value:
+            total = total - current_value
+        else:
+            total = total + current_value
+    total = total + roman_list[roman_string[-1]]
+    return total
