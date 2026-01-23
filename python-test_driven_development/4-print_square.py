3adb083792c848456ca82276f74644c298e78683
AdeleM-prog <12043@holbertonstudents.com>
2026-01-23 19:08:05 +0000

    crea 4-main + 4-print.py + 4-print.txt

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -4,8 +4,19 @@
 
 def print_square(size):
     """size is the size length of the square
-    it must be an integer, otherwise raise a TypeError exception with the message size must be an integer
-    if size is less than 0, raise a ValueError exception with the message size must be >= 0
-    if size is a float and is less than 0, raise a TypeError exception with the message size must be an integer
-    
-
+    it must be an integer, otherwise raise a TypeError
+    exception with the message size must be an integer
+    if size is less than 0, raise a ValueError exception
+    with the message size must be >= 0
+    if size is a float and is less than 0, raise a TypeError
+    exception with the message size must be an integer"""
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("{}".format('#'), end="")
+        print()
