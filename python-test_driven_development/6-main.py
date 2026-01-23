4cec74d1fc1715108b2fe803539c0f4838e45c6b
AdeleM-prog <12043@holbertonstudents.com>
2026-01-23 22:31:15 +0000

    test checker

--- python-test_driven_development/6-main.py
+++ python-test_driven_development/6-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-max_integer = __import__('6-max_integer').max_integer
+max_integer = __import__('6-max_integer_test').max_integer
 """Module to find the max integer in a list"""
 
 
--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -2,7 +2,6 @@
 """Unittest for max_integer([..])"""
 
 import unittest
-max_integer = __import__('6-max_integer_test').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
