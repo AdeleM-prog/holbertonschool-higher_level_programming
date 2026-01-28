b8776fadef2eb160b8c0db5d4b81485e6c3e0342
AdeleM-prog <12043@holbertonstudents.com>
2026-01-28 08:37:44 +0000

    correction commentaires

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,15 +2,19 @@
 """a class Square that defines a square"""
 
 class Square:
+    """ a class square"""
     def __init__(self, size=0):
+        """raises type erreor when not an integer
+        raises valueError if under
+        Private instance attribute
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        """Private instance attribute"""
         self.__size = size
 
-"""Public instance method
-that returns the current square area"""
     def area(self):
+        """Public instance method
+        that returns the current square area"""
         return self.__size * self.__size
