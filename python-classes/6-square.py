0be278b99ffa7e4ad4182767a33b5fe2d4250217
AdeleM-prog <12043@holbertonstudents.com>
2026-01-28 16:33:32 +0000

    correction commentaires

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -6,18 +6,21 @@ class Square:
     """ a class square"""
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
-    """property that calls a function like
-    an attribute"""
     @property
     def size(self):
+        """
+        property that calls a function like
+        an attribute
+        """
         return self.__size
 
-    """raises type erreor when not an integer
-    raises valueError if under
-    Private instance attribute
-    """
     @size.setter
     def size(self, value):
+        """
+        raises type erreor when not an integer
+        raises valueError if under
+        Private instance attribute
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -25,33 +28,34 @@ class Square:
         """Private instance attribute"""
         self.__size = value
 
-    """method position gives an attribute position"""
-    def position(self):
-        self.position = position
-
-    """property that calls a function like
-    an attribute"""
     @property
     def position(self):
+        """
+        property that calls a function like
+        an attribute
+        """
         return self.__position
 
-    """raises type erreor when not an integer
-    raises valueError if under
-    """
     @position.setter
     def position(self, value):
+        """
+        raises type erreor when not an integer
+        raises valueError if under
+        """
         if not isinstance(value, tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
 
-    """Public instance method
-    that returns the current square area"""
     def area(self):
+        """
+        Public instance method that returns the square area
+        """
         return self.__size * self.__size
 
-    """Public instance method
-    that prints in stdout the square
-    with the character #"""
     def my_print(self):
+        """
+        Public instance method that prints in stdout the square
+        with the character #
+        """
         if self.size == 0:
             print()
         for i in range(self.size):
