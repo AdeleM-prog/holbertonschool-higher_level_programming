91ca958e986623061478bbb21e19de303b432dab
AdeleM-prog <12043@holbertonstudents.com>
2026-01-28 10:53:33 +0000

    implementation 4-square + pycodestyle

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -4,16 +4,23 @@
 
 class Square:
     def __init__(self, size=0):
+        self.size = size
         """raises type erreor when not an integer
         raises valueError if under
         Private instance attribute
         """
-        if not isinstance(size, int):
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         """Private instance attribute"""
-        self.__size = size
+        self.__size = value
 
     """Public instance method
     that returns the current square area"""
