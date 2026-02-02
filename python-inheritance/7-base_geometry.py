456b9fc95c83e6eb4ec47e050b2012f7015863ec
AdeleM-prog <12043@holbertonstudents.com>
2026-02-02 17:08:47 +0000

    crea 7-main + 7-base + pycodestyle

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""no module imported"""
+
+
+class BaseGeometry:
+    """Public instance method
+    that raises an Exception with the message area()
+    is not implemented
+    Public instance method that validates value
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
