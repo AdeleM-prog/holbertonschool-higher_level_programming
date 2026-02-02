073e29ca0fd8ef1d3008e7ed243d09d12f2c2fbe
AdeleM-prog <12043@holbertonstudents.com>
2026-02-02 15:47:35 +0000

    crea 2- main + 2-is_same_class + pycodestyle

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" no module imported"""
+
+
+def is_same_class(self, a_class):
+    """a function that returns True if the object
+    is exactly an instance of the specified class;
+    otherwise False"""
+    return type(self) == a_class
