6ec42e16a4c71483a9c99e6a12cc59e6630394b0
AdeleM-prog <12043@holbertonstudents.com>
2026-02-03 09:26:23 +0000

    implementation 9-rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,25 +2,30 @@
 """no module imported"""
 
 
-class Rectangle:
+class BaseGeometry:
     """
-    Instantiation with width and height:
-    width and height must be private
-    width and height must be positive integers, validated by integer_validator
     Public instance method
     that raises an Exception with the message area()
     is not implemented
     Public instance method that validates value
     """
-    def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-
     def area(self):
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+        if type(value) is not int:
+                raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """ Instantiation with width and height
+    width and height must be private.
+    width and height must be positive integers,
+    validated by integer_validator
+    """
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
