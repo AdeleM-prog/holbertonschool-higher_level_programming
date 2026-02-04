26784a27cee7ba073f2fef171dd6cc519fda6bba
AdeleM-prog <12043@holbertonstudents.com>
2026-02-04 10:05:43 +0000

    correction checker

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """ a function that returns True if the object is an instance
-    of a class that inherited (directly or indirectly) from the
+    of a class that inherited (directly or indirectly) the
     specified class ; otherwise False
     """
     return issubclass(type(obj), a_class) and type(obj) != a_class
