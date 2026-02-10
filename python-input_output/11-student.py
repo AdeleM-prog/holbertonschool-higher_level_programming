8b64c7b197954655d1bd364711fd8087bf950821
AdeleM-prog <12043@holbertonstudents.com>
2026-02-10 19:35:14 +0000

    crea 11-main + 11-student a finaliser

---
+++ python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(type(x) is str for x in attrs):
+            student_dict = dict()
+            for key in attrs:
+                if key in self.__dict__:
+                    student_dict[key] = self.__dict__[key]
+            return student_dict
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        
