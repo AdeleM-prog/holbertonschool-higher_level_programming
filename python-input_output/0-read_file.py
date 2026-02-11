ef2c463c6b21d3c6398e7db80cfb424c16d71ecb
AdeleM-prog <12043@holbertonstudents.com>
2026-02-11 08:22:57 +0000

    add docstrings

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" This module provides a function that reads a text file (UTF8) and prints it to stdout"""
+""" This module provides a function that reads a text file (UTF8)
+and prints it to stdout"""
+
 
 def read_file(filename=""):
     """
--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+""" This module provides a function that writes a string to a
+text file (UTF8) and returns the number of characters written
+"""
+
 
 def write_file(filename="", text=""):
+    """ function that writes a string to a text file (UTF8)
+    returns the number of characters written
+    """
     with open(filename, "w", encoding="utf-8") as f:
         nb = f.write(text)
         return nb
