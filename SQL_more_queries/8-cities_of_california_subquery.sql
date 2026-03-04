4e6aef002af6160d3c4ec842b0ad0ccb7a1e0d0e
AdeleM-prog <12043@holbertonstudents.com>
2026-03-04 14:52:01 +0000

    correction checker

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,9 @@
 -- Task 8 Project 1
 -- script that lists all the cities of California that can be found in the database hbtn_0d_usa
 
-SELECT name, id
+SELECT id, name
 FROM cities
-WHERE state_id IN (
+WHERE state_id = (
     SELECT id
     FROM states
     WHERE name = 'California'
