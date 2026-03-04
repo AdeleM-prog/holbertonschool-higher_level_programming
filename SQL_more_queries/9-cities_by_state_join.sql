23a4bae2ddadf28e3893d2d67b0d48d84e40c731
AdeleM-prog <12043@holbertonstudents.com>
2026-03-04 15:26:20 +0000

    crea 9-cities_by_state_join

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Task 9 Project 1
+-- script that lists all cities contained in the database hbtn_0d_usa
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id;
