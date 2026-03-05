7ba5f9241e4fcd47d7bfd2e86d78d889991fc400
AdeleM-prog <12043@holbertonstudents.com>
2026-03-05 14:09:35 +0000

    crea 11-genre_id_all_shows

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,7 @@
--- Task 10 Project 10
--- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-
+-- Task 11 Project 1
+-- script that lists all shows contained in the database hbtn_0d_tvshows
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
