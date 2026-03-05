fe28267675fe55621f5380c1e69165206fbc9f90
AdeleM-prog <12043@holbertonstudents.com>
2026-03-05 14:12:40 +0000

    correction checker

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,6 @@
 -- Task 11 Project 1
 -- script that lists all shows contained in the database hbtn_0d_tvshows
+
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
