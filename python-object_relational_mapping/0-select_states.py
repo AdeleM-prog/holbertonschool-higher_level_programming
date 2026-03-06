bd263f32cf12102a0d2b3298d3e9c628276cd31d
AdeleM-prog <12043@holbertonstudents.com>
2026-03-06 12:08:09 +0100

    correction checker

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,21 +10,21 @@ if __name__ == "__main__":
 
     """storing arguments in variables"""
     User = sys.argv[1]
-    Passwd = sys.argv[2]
-    Dtbs = sys.argv[3]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
 
     """connexion to the server"""
     db = MySQLdb.connect(
         host='localhost',
         port=3306,
         user=User,
-        password=Passwd,
-        database=Dtbs
+        password=passwd,
+        database=db
         )
 
     """cursor interpreting the command and executing the SQL query"""
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC;")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
     rows = cur.fetchall()
 
     """catching the lines from the result in order to print them"""
