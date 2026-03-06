09de291061fd75f3d7e6ad5d295e6167d4aff7f7
AdeleM-prog <12043@holbertonstudents.com>
2026-03-06 13:28:49 +0100

    crea 1-filter_states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+MySQL imported to connect to the server and exectute query
+sys imported to catch the arguments
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * "
+        "FROM states "
+        "WHERE states.name LIKE 'N%' "
+        "ORDER BY states.id ASC")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
