58a1c8bb1675ebff6ec72556374a14e01dba72e8
AdeleM-prog <12043@holbertonstudents.Com>
2026-03-20 13:13:05 +0000

    correction syntaxe

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
-const nb_args = process.argv.length - 2
+const nb_args = process.argv.length - 2;
 if (nb_args == 0) {
-  console.log('No argument')}
-else if (nb_args == 1) {
-  console.log('Argument found')}
-else {
-  console.log('Arguments found')}
+  console.log('No argument');
+} else if (nb_args == 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
