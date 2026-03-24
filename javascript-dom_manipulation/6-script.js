3acbef05c31009c2c7e7130ae1d3350f2619cab9
AdeleM-prog <12043@holbertonstudents.com>
2026-03-24 14:30:53 +0000

    crea 6-script.js + implement url fetch

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+fetch("https://swapi-api.hbtn.io/api/people/5/?format=json")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    const character = document.querySelector("#character");
+    character.textContent = data.name;
+  });
