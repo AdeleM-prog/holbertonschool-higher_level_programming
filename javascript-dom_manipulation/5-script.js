bcaf6defb5a8678ba106f8169d550bb4deef13b3
AdeleM-prog <12043@holbertonstudents.com>
2026-03-24 14:19:24 +0000

    crea 5-script.js + implement header update

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const update_header = document.querySelector("#update_header");
+update_header.addEventListener("click", function () {
+    const header = document.querySelector("header");
+    header.textContent = "New Header!!!";
+})
