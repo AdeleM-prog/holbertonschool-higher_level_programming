3ecd7ac5d166a0804a77cf245904318c37fd7350
AdeleM-prog <12043@holbertonstudents.com>
2026-03-24 13:43:42 +0000

    crea 3-script.js + implment class change

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const toggle_header = document.querySelector("#toggle_header");
+toggle_header.addEventListener("click", function () {
+    const header = document.querySelector("header");
+    if (header.classList.contains("red")) {
+        header.classList.remove("red");
+        header.classList.add("green");
+    }
+    else {
+        header.classList.remove("green");
+        header.classList.add("red");
+    }
+})
